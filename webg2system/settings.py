fcdbadc6365f348c6aff2f06bae49c421eff9218
geo5 <ricardo.silva@meteo.pt>
2012-06-08 13:32:28 +0100

    added the local settings template file

--- webg2system/settings.py
+++ webg2system/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Ricardo Silva', 'ricardo.silva@meteo.pt'),
+    ('Ricardo Silva', 'ricardo.silva@meteo.pt'),
 )
 
 MANAGERS = ADMINS
@@ -54,7 +54,7 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 #MEDIA_ROOT = os.path.join(SITE_ROOT, 'assets')
-MEDIA_ROOT = '/home/geo2/Public/'
+MEDIA_ROOT = '/home/geo5/Public/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/geo2/dev/webg2system/sitestatic'
+STATIC_ROOT = '/home/geo5/webg2system/sitestatic'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -213,3 +213,9 @@ LOGGING = {
             },
         },
 }
+
+# local settings overrides. MUst be the last import!
+try:
+    from settings_local import *
+except ImportError:
+    pass
