7f5ae52e1e9eeb44c1d9c848660c2092c57e8c73
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2012-07-25 16:19:01 +0100

    Tweaking the fabfile to make it more flexible.

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,40 @@ remote servers at once.
 
 from fabric.api import env, local, cd, run, settings
 
-env.hosts = ['geoland@180.180.30.98']
+# define hosts here
+HOSTS = {
+    '180.180.30.98' : {
+        'user' : 'geoland', 
+        'code_dir' : '/home/geoland/silvar/webg2system'
+    },
+    '180.180.30.99' : {
+        'user' : 'geo5',
+        'code_dir' : '/home/geo5/silvar/webg2system'
+    },
+    '193.137.20.109' :{
+        'user' : 'geoland2',
+        'code_dir' : '/home/geoland2/webg2system'
+    },
+}
+
+def _prepare_env_hosts():
+    '''
+    Return a list suitable for fabric's env.hosts variable.
+
+    The returned list is constructed from the global HOSTS
+    variable.
+    '''
+
+    global HOSTS
+    host_list = []
+    for host, settings in HOSTS.iteritems():
+        host_string = '%s@%s:%s' % (settings['user'], 
+                                    host, 
+                                    settings.get('port', 22))
+        host_list.append(host_string)
+    return host_list
+
+env.hosts = _prepare_env_hosts()
 
 def prepare_deploy():
     commit()
@@ -21,7 +54,13 @@ def push():
     local('git push origin master')
 
 def deploy():
-    code_dir = '/home/geoland/silvar/webg2system'
+    '''
+    Deploy the latest changes present in the git repository to the
+    defined hosts.
+    '''
+
+    global HOSTS
+    code_dir = HOSTS[env.host]['code_dir']
     with settings(warn_only=True):
         if run('test -d %s' % code_dir).failed:
             run('git clone git@github.com:ricardogsilva/webg2system.git %s' % \
