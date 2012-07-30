3c28c4886c83a7da2eacd3510e32ceb11da36ac8
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2012-07-30 17:39:33 +0100

    started creating a fab function for checking out and compiling extra_code

--- fabfile.py
+++ fabfile.py
@@ -5,8 +5,18 @@
 A fabric fabfile in order to automate system installation/upgrade on several
 remote servers at once.
 """
+import sys
+sys.path.append('webg2system')
 
 from fabric.api import env, local, cd, run, settings
+
+from django.core.management import setup_environ
+import settings as s
+setup_environ(s)
+
+import systemsettings.models as sm
+
+
 
 # define hosts here
 HOSTS = {
@@ -64,3 +74,45 @@ def deploy():
     with cd(code_dir):
         run('git pull origin master')
         run('touch webg2system/wsgi.py')
+
+#FIXME - Finish this method
+def get_private_code(repository_url=None):
+    '''
+    Checkout the latest changes to the main product algorithms.
+    '''
+
+    if repository_url is None:
+        repository_url = 'http://gridpt13.meteo.pt/ciac/G2System/trunk'
+    ext_code = {
+        'GRIB2HDF5_g2' : 'PRE_PROCESS/GRIB2HDF5_g2/GRIB2HDF5_g2_v%s',
+        'LRIT2HDF5_g2' : 'PRE_PROCESS/LRIT2HDF5_g2/LRIT2HDF5_g2_v%s',
+        'CMa_g2' : 'PRODUCTS/CMa_g2/CMa_g2_v%s',
+        'DSLF_g2' : 'PRODUCTS/DSLF_g2/DSLF_g2_v%s',
+        'DSSF_g2' : 'PRODUCTS/DSSF_g2/DSSF_g2_v%s',
+        'GSA_g2' : 'PRODUCTS/GSA_g2/GSA_g2_v%s',
+        'GSA_PP_g2' : 'PRODUCTS/GSA_PP_g2/GSA_PP_g2_v%s',
+        'LST_g2' : 'PRODUCTS/LST_g2/LST_g2_v%s',
+        'REF_g2' : 'PRODUCTS/REF_g2/REF_g2_v%s',
+        'SAT_DATA_STAT_g2' : 'PRODUCTS/SAT_DATA_STAT_g2/SAT_DATA_STAT_g2_v%s',
+        'SA_g2' : 'PRODUCTS/SA_g2/SA_g2_v%s',
+        'SWI_g2' : 'PRODUCTS/SWI_g2/SWI_g2_v%s',
+    }
+    defined_packages = set()
+    for pack in sm.Package.objects.all():
+        code_name = None
+        version = None
+        for ei in pack.packageextrainfo_set.all():
+            if ei.name == 'codeName':
+                code_name = ei.string
+            elif ei.name == 'version':
+                version = ei.string
+        defined_packages.add((code_name, version))
+    for tup in defined_packages:
+        code_name, version = tup
+        extra_url = ext_code.get(code_name)
+        if extra_url is not None:
+            url = '/'.join((repository_url, extra_url % version))
+            print('url: %s' % url)
+
+
+
