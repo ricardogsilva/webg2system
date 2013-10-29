3c20a11eea1603b7aeb1b11dc559b3568fdbcb77
ricardogsilva <ricardo.garcia.silva@gmail.com>
2013-10-29 12:01:36 +0000

    made the script that backs up the catalog database a bit more robust

--- extra_code/sdi_backup/backup_database.py
+++ extra_code/sdi_backup/backup_database.py
@@ -2,45 +2,30 @@
 #-*- coding: utf-8 -*-
 
 """
-A fabric fabfile to automate backing up geonetwork's database into the archives.
+A fabric fabfile to automate backing up geonetwork's database into the archive
 
 It is to be executed as:
 
-    $ fab -f backup_database.py execute_backup:db_name='geonetwork',db_user='geonetwork',backup_name='teste.sql'
+    $ fab -f backup_database.py execute_backup:db_name='geonetwork',db_user='geonetwork'
 """
 
 import sys
 import os
+import datetime as dt
+
 from fabric.api import env, local, cd, lcd, run, settings, sudo
 
-def execute_backup(db_name='geonetwork_new', 
-                   db_user='geonetwork_new',
-                   backup_name='geonetwork_db_backup.sql',
-                   backup_dir='/home/geo3/BACKUPS'):
+def execute_backup(db_name, db_user):
+    now = dt.datetime.utcnow()
+    backup_name = now.strftime('geonetwork_db_backup_%Y_%m_%d_%H_%M.sql')
     destination_host = '192.168.151.29'
     destination_user = 'g2user'
     destination_path = '/media/Data3/geoland2/BACKUPS'
-    with lcd(backup_dir):
-        _dump_database(db_name, db_user, backup_name)
-        _rsync_dump(backup_name, destination_user, 
-                   destination_host, destination_path)
-        _delete_dump(backup_name)
-
-def _dump_database(db_name, db_user, backup_name):
-    '''
-    Execute the external pg_dump utility in order to create a backup of the
-    database.
-    '''
-    local('pg_dump -U %s %s > %s' % (db_user, db_name, backup_name))
-
-def _rsync_dump(backup_name, dst_user, dst_host, dst_path):
-    '''
-    rsync the backup file to the archive.
-    '''
-    local('rsync %s %s@%s:%s' % (backup_name, dst_user, dst_host, dst_path))
-
-def _delete_dump(backup_name):
-    '''
-    Delete the local backup.
-    '''
-    local('rm %s' % backup_name)
+    local_backup_dir = os.path.expanduser('~/temp_backups')
+    if not os.path.isdir(local_backup_dir):
+        os.makedirs(local_backup_dir)
+    with lcd(local_backup_dir):
+        local('pg_dump -U %s %s > %s' % (db_user, db_name, backup_name))
+        local('rsync %s %s@%s:%s' % (backup_name, destination_user,
+              destination_host, destination_path))
+    local('rm -rf %s' % local_backup_dir)
