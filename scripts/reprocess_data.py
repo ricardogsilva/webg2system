1c25f930d15dbedc94c2d281c8706770b47e624c
ricardogsilva <ricardo.garcia.silva@gmail.com>
2013-10-29 12:00:59 +0000

    worked in a script to reprocess data

--- scripts/reprocess_data.py
+++ scripts/reprocess_data.py
@@ -3,12 +3,22 @@
 
 '''
 A script to reprocess some timeslots from the processing lines.
+
+It should be run like this:
+
+    $ python reprocess_data.py -k populateCSW:True lst_metadata 201301010000 \
+            201301312300 1
+
+This example will reprocess the lst_metadata package from 2013/01/01 00:00 
+until 2013/01/31 23:00 using an hourly frequency. It will supply the 
+populateCSW argument to the package, using a value of True.
 '''
 
 import os
 import sys
-import datetime
+import datetime as dt
 import logging
+import argparse
 
 from django.core.management import setup_environ
 sys.path.append('/home/geo2/dev/webg2system/webg2system')
@@ -17,30 +27,34 @@ setup_environ(s)
 
 import operations.models as om
 
-if __name__ == '__main__':
-    products = ['swi',]
-    packages = [
-        {
-            'name' : '%s_metadata',
-            'kwargs' : {
-                'send_to_csw' : True,
-            }
-        },
-    ]
+def get_kwarg(item):
+    key,value = item.split(':')
+    value = eval(value)
+    return {key: value}
 
-    for day in range (20, 21):
-        print('---- day %i ----' % day)
-        for hour in range(0, 1):
-            print('\t---- hour %i ----' % hour)
-            for product in products:
-                print('\t\t---- product %s ----' % product)
-                for pack_dict in packages:
-                    pack_name = pack_dict['name'] % product
-                    print('\t\t\t---- package: %s ----' % pack_name)
-                    ts = datetime.datetime(2013, 10, day, hour)
-                    rp = om.RunningPackage(settings=pack_name, area='.*',
-                                           timeslot=ts)
-                    result = rp.run(log_level=logging.INFO,
-                                    **pack_dict.get('kwargs', dict()))
-                    #del rp
-                    print('\t\t\t\t ---- result: %s ----' % result)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('package')
+    parser.add_argument('start_timeslot')
+    parser.add_argument('end_timeslot')
+    parser.add_argument('frequency', default=1, type=int, help='Frequency '
+                        'for the pakcages (in hours)')
+    parser.add_argument('-k', '--package_kwargs', type=get_kwarg,
+                        action='append')
+    args = parser.parse_args()
+    package_kwargs = dict()
+    [package_kwargs.update(dd) for dd in args.package_kwargs]
+    timeslots = []
+    start_ts = dt.datetime.strptime(args.start_timeslot, '%Y%m%d%H%M')
+    end_ts = dt.datetime.strptime(args.end_timeslot, '%Y%m%d%H%M')
+    delta = dt.timedelta(hours=args.frequency)
+    current_ts = start_ts
+    while current_ts <= end_ts:
+        timeslots.append(current_ts)
+        current_ts += delta
+    for index, ts in enumerate(timeslots):
+        print('processing timeslot %s (%i/%i)' % \
+              (ts.strftime('%Y/%m/%d %H:%M'), index+1, len(timeslots)))
+        rp = om.RunningPackage(settings=args.package, area='.*',
+                               timeslot=ts)
+        result = rp.run(log_level=logging.INFO, **package_kwargs)
